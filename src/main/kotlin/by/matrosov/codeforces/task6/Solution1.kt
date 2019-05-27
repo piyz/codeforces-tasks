94258658d937be16f3282e1542ffbfd9c23ece5f
Andrey Matrosov <matrosovs2008@yandex.ru>
2019-05-27 23:24:32 +0300

    update task6

--- src/main/kotlin/by/matrosov/codeforces/task6/Solution1.kt
+++ src/main/kotlin/by/matrosov/codeforces/task6/Solution1.kt
@@ -1,4 +1,4 @@
-package by.matrosov.codeforces.task6
+
 
 data class Interval(val left: Int, val right: Int, var indexes: Set<Int>): Comparable<Interval>{
     override fun compareTo(other: Interval): Int {
@@ -46,66 +46,71 @@ fun check(i1: Interval, i2: Interval): Interval?{
 
 fun main() {
 
+    val list = mutableListOf<Interval>()
 
-    val a = Interval(3,5, setOf(0))
-    val b = Interval(2,3, setOf(1))
-    val c = Interval(2,3, setOf(2))
-    val d = Interval(0,6, setOf(3))
-    val e = Interval(0,3, setOf(4))
-    val f = Interval(4,4, setOf(5))
-    val g = Interval(5,10, setOf(6))
+    val numberOfRequests = readLine()!!.toInt()
+    for (n in 0 until numberOfRequests){
 
-    //val list = mutableListOf(r,t,y)
-    val list = mutableListOf(a,b,c,d,e,f,g)
-    val count = list.size - 1
-
-    var i = -1
-    var j: Int
-    var flag = true
-    while (true){
-
-        i++
-        if (i == list.size - 1){
-            break
+        val numberOfIntervals = readLine()!!.toInt()
+        for (p in 0 until numberOfIntervals){
+            val string = readLine()!!.split(" ")
+            val l = string[0].toInt()
+            val r = string[1].toInt()
+            list.add(Interval(l,r, setOf(p)))
         }
-        j=i+1
+
+        val count = list.size - 1
+
+        var i = -1
+        var j: Int
+        var flag = true
         while (true){
 
-            //println("check " + list[i] + " and " + list[j])
-
-            val first = list[i]
-            val second = list[j]
-
-            val che = check(list[i], list[j])
-            if (che != null){
-                list.remove(first)
-                list.remove(second)
-                list.add(che)
-                flag = false
-            }else{
-                j++
+            i++
+            if (i == list.size - 1){
+                break
             }
-            if (!flag || j == list.size) break
-        }
+            j=i+1
+            while (true){
 
-        if (!flag){
-            i = -1
-            flag = true
-        }
-    }
+                //println("check " + list[i] + " and " + list[j])
 
-    //println(list)
-    if (list.size == 1){
-        println(-1)
-    }else{
-        for (k in 0..count){
-            if (list[0].indexes.contains(k)){
-                print(1)
-            }else{
-                print(2)
+                val first = list[i]
+                val second = list[j]
+
+                val che = check(list[i], list[j])
+                if (che != null){
+                    list.remove(first)
+                    list.remove(second)
+                    list.add(che)
+                    flag = false
+                }else{
+                    j++
+                }
+                if (!flag || j == list.size) break
             }
-            print(" ")
+
+            if (!flag){
+                i = -1
+                flag = true
+            }
         }
+
+        //println(list)
+        if (list.size == 1){
+            println(-1)
+        }else{
+            for (k in 0..count){
+                if (list[0].indexes.contains(k)){
+                    print(1)
+                }else{
+                    print(2)
+                }
+                print(" ")
+            }
+            println()
+        }
+        list.clear()
     }
 }
 
